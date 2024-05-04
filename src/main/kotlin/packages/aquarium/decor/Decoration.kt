0f524050e0c6f194cb4d9cf50c0109dcdabfef9b
rigelabs <rigelsolutions@outlook.com>
2024-05-04 19:20:40 +0300

    full commit

--- src/main/kotlin/packages/aquarium/decor/Decoration.kt
+++ src/main/kotlin/packages/aquarium/decor/Decoration.kt
@@ -3,11 +3,11 @@ package packages.aquarium.decor
 data class Decoration(val rocks: String, val wood: String, val diver: String) {
 }
 fun makeDecoration(){
-    val decoration = Decoration(rocks = "Granite")
+    val decoration = Decoration(rocks = "Granite", wood = "Magohany", diver = "Merry")
     print(decoration)
-    val decoration2 = Decoration(wood = "slate")
+    val decoration2 = Decoration(wood = "slate", rocks = "Graphite", diver = "Glory")
     println(decoration2)
 
-    val decoration3 = Decoration(diver = "slate")
+    val decoration3 = Decoration(diver = "slate", rocks = "Diamond", wood = "Tick")
     println(decoration3)
 }
